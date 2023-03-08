3bfdec6dc17ac99c03ec16134569656eb2785212
Székely Balázs Csaba <szekelyb91@gmail.com>
2023-03-08 21:22:53 +0100

    linkelések, kezdőkommentek

---
+++ src/style.css
@@ -0,0 +1,9 @@
+/*
+* File: style.css
+* Author: Székely Balázs Csaba
+* Copyright: 2023, Székely Balázs Csaba
+* Group: SZOFT 1/I/E
+* Date: 2023-03-08
+* Github: https://github.com/BlaiseD91/
+* Licenc: GNU GPL
+*/
